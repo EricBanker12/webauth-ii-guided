5fbeaa0511a22688b345e5ae696a4a938206fe17
ericbanker12 <ericbanker12@gmail.com>
2019-11-12 13:05:17 -0600

    add dependencies, add cookies, handle sessions

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session')
+const SessionStore = require('connect-session-knex')(session)
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -10,12 +12,29 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session({
+  name: 'web23',
+  secret: process.env.SECRET || 'secret key',
+  cookie: {
+    maxAge: 1000 * 60 * 60, // 1 hour in milliseconds
+    secure: process.env.NODE_ENV === 'development' ? false : true,
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: process.env.NODE_ENV === 'development' ? true : false,
+    store: new SessionStore({
+      knex: require('../database/dbConfig'),
+      clearInterval: 1000* 60 * 10, // delete expired sessions every 10 min 
+      tablename: 'user_sessions',
+      createtable: true,
+    }),
+  }
+}))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+  res.json({ api: 'up', session: req.session });
 });
 
 module.exports = server;
---
+++ notes.md
@@ -0,0 +1,14 @@
+# auth
+- client connect to api 
+- - not necessarily same user, or multiple clients per user
+- server does not remember client across requests
+
+## cookies
+- a container of data
+- browser automatically send on every request to domain associated with cookie
+
+server send header to client requesting to store a cookie
+
+## sessions
+- like a database
+- store data on server
